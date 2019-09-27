5c6e6d5cea9028844c9b625ee27baaff8cef0f60
gisonyeung <gisonyeung@gmail.com>
2019-09-27 19:05:46 +0800

    add(method):增加解法

--- middle/39-combination-sum.js
+++ middle/39-combination-sum.js
@@ -50,7 +50,7 @@ var combinationSum = function(candidates, target) {
   let i = 0, len = candidates.length, minNum = candidates[len - 1];
 
   for (i; i < len; i++) {
-    console.log(`start:${i}`);
+    // console.log(`start:${i}`);
     dfs([], i, target);
   }
 
@@ -58,19 +58,18 @@ var combinationSum = function(candidates, target) {
     if (restSum < 0 || start >= len) return;
 
     let nextRestNum = restSum - candidates[start];
-    console.log('nextRestNum', restSum, '-', candidates[start], '=', nextRestNum);
+    // console.log('nextRestNum', restSum, '-', candidates[start], '=', nextRestNum);
 
     if (nextRestNum === 0) { // 结果
-      console.log('结果', (curRes.concat(candidates[start])));
+      // console.log('结果', (curRes.concat(candidates[start])));
       res.push(curRes.concat(candidates[start]));
-
       // 上一步的 restSum 不为初始值则继续尝试右移下标，否则终止，因为这相当于和根循环重复了
       return restSum !== target ? dfs(curRes, ++start, restSum) : null;
     }
     
     // 如果下一步剩余的总和已经比集合里最小值还要小了，说明这一次相减会导致没结果，那就应该跳过这一步
     if (nextRestNum < minNum) { 
-      console.log('nextRestNum < minNum', curRes);
+      // console.log('nextRestNum < minNum', curRes);
       // 上一步的 restSum 不为初始值则继续尝试右移下标，否则终止，因为这相当于和根循环重复了
       return restSum !== target ? dfs(curRes, ++start, restSum) : null;
     } else if (nextRestNum < candidates[start]) { // 下一步剩余的总和比当前扫描的值小，则可以将下标右移指向下一个扫描值
@@ -81,7 +80,7 @@ var combinationSum = function(candidates, target) {
 
     // 如果当前已经是广度遍历，则不能再跳过该步了，否则接下来外层的广度遍历会和下面的循环陈洪福
     if (restSum !== target && restSum > candidates[start] && start < len - 1 && !isBFS) {
-      console.log('跳过上面的解法，继续广度', curRes, start + 1, len);
+      // console.log('跳过上面的解法，继续广度', curRes, start + 1, len);
       for (let j = start + 1; j < len; j++) {
         dfs(curRes, j, restSum, true);
       }
@@ -89,17 +88,54 @@ var combinationSum = function(candidates, target) {
 
   }
 
-  console.log('\nres', res, '\n');
+  // console.log('\nres', res, '\n');
+  return res;
+};
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ * 64ms 100.00%
+ * 36.3mb 83.33%
+ * 极致简化版
+ */
+var combinationSum = function (candidates, target) {
+  // 由大到小排序
+  candidates.sort((a, b) => b - a);
+
+  let res = [], path = [];
+  let len = candidates.length, minNum = candidates[len - 1];
+
+  get_combin(candidates, target, 0, path);
+
+  function get_combin(candidates, target, start, path) {
+    if (target == 0) {
+      return res.push(path.slice());
+    }
+
+    if (target < minNum) return;
+
+    for (let i = start; i < len; i++) {
+      path.push(candidates[i]);
+      get_combin(candidates, target - candidates[i], i, path);
+      // 无论是该路径大于target还是等于target，都需要对其删除最后一个元素，进行其余支路的搜索
+      path.pop();
+    }
+  }
+
   return res;
 };
 
 
 
-combinationSum([3,2,8], 18);
-combinationSum([7,3,2], 18);
-combinationSum([1,2], 4);
-combinationSum([1,2], 2);
-combinationSum([], 7);
-combinationSum([2,3,6,7], 0);
+
+
+// combinationSum([3,2,8], 18);
+// combinationSum([7,3,2], 18);
+// combinationSum([1,2], 4);
+// combinationSum([1,2], 2);
+// combinationSum([], 7);
+// combinationSum([2,3,6,7], 0);
 combinationSum([2,3,6,7], 7);
-combinationSum([2,3,5], 8);
+// combinationSum([2,3,5], 8);
