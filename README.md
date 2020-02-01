49566987a13a8d577cf02fb717e784b27d1aa426
gisonyeung <gisonyeung@gmail.com>
2020-02-01 19:16:01 +0800

    add(hard): 4.寻找两个有序数组的中位数

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 题库：[https://leetcode-cn.com/problemset/all/](https://leetcode-cn.com/problemset/all/)
 
 ## 已刷题目记录
-已刷题目总数：38
+已刷题目总数：39
 
 **简单(easy)**[10]
 - 1.两数之和: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/easy/1-two-sum.js) [查看原题](https://leetcode-cn.com/problems/two-sum/)
@@ -39,7 +39,8 @@
 - ⭐215.数组中的第K个最大元素【堆|分治算法】: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/middle/215-kth-largest-element-in-an-array.js) [查看原题](https://leetcode-cn.com/problems/kth-largest-element-in-an-array/)
 - 240.搜索二维矩阵 II: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/middle/240-search-a-2d-matrix-ii.js) [查看原题](https://leetcode-cn.com/problems/search-a-2d-matrix-ii/)
 
-**困难(hard)**[10]
+**困难(hard)**[11]
+- 4.寻找两个有序数组的中位数【数组|二分查找|分治算法】: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/hard/4-median-of-two-sorted-arrays.js) [查看原题](https://leetcode-cn.com/problems/median-of-two-sorted-arrays/)
 - 10.正则表达式匹配【字符串|动画规划|回溯算法】: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/hard/10-regular-expression-matching.js) [查看原题](https://leetcode-cn.com/problems/regular-expression-matching/)
 - ⭐23.合并K个排序链表【堆|链表|分治算法】: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/hard/23-merge-k-sorted-lists.js) [查看原题](https://leetcode-cn.com/problems/merge-k-sorted-lists/)
 - ⭐32.最长有效括号【字符串|动态规划】: [查看代码](https://github.com/gisonyeung/leetcode-traning/blob/master/hard/32-longest-valid-parentheses.js) [查看原题](https://leetcode-cn.com/problems/longest-valid-parentheses/)
---
+++ hard/4-median-of-two-sorted-arrays.js
@@ -0,0 +1,74 @@
+/**
+ * @name 寻找两个有序数组的中位数
+ * @url https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ * @tags 数组、二分查找、分治算法
+ * @description
+ * ```
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * 
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 
+ * 示例 1:
+ * 
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 
+ * 则中位数是 2.0
+ * 
+ * 
+ * 示例 2:
+ * 
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * 
+ * 
+ * ```
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * 112ms 97.85%
+ * 38.7mb 95.40%
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  let n = nums1.length, m = nums2.length;
+  let k1 = Math.floor((n + m + 1) / 2);
+  let k2 = Math.floor((n + m + 2) / 2);
+
+  return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, k1) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, k2)) / 2;
+};
+
+function getKth(nums1, start1, end1, nums2, start2, end2, k) {
+  let len1 = end1 - start1 + 1; 
+  let len2 = end2 - start2 + 1;
+
+  // 让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+  if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+  if (len1 === 0) return nums2[start2 + k - 1];
+
+  if (k === 1) return Math.min(nums1[start1], nums2[start2]);
+
+  let k_harf = Math.floor(k / 2);
+  let _start1 = start1 + Math.min(len1, k_harf) - 1;
+  let _start2 = start2 + Math.min(len2, k_harf) - 1;
+  
+  // 舍弃更小的数组
+  if (nums1[_start1] > nums2[_start2]) {
+    return getKth(nums1, start1, end1, nums2, _start2 + 1, end2, k - (_start2 - start2 + 1));
+  } else {
+    return getKth(nums1, _start1 + 1, end1, nums2, start2, end2, k - (_start1 - start1 + 1));
+  }
+}
+
+console.log(findMedianSortedArrays([1,2], [4]));
+console.log(findMedianSortedArrays([1,2], [3,4]));
+console.log(findMedianSortedArrays([1,2], [4,5]));
+console.log(findMedianSortedArrays([1,2], [3,4,5]));
+console.log(findMedianSortedArrays([1,2], [-1,3]));
+console.log(findMedianSortedArrays([1,3], [2,4,5,6]));
