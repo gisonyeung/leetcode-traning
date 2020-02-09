0d3caf4a63165e57f7969c3beca6fe7d8d0cab5f
gisonyeung <gisonyeung@gmail.com>
2020-02-09 18:52:33 +0800

    feat: add star

--- hard/85-maximal-rectangle.js
+++ hard/85-maximal-rectangle.js
@@ -2,6 +2,7 @@
  * @name 最大矩形
  * @url https://leetcode-cn.com/problems/maximal-rectangle/
  * @tags 栈、数组、哈希表、动态规划
+ * @star
  * @description
  * ```
  * 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
